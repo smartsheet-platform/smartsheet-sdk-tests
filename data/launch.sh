598422014cdd22d6c9ad7789167e5b55f2c38225
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-11-09 12:59:47 -0800

    Add intellij files and launch file

--- data/launch.sh
+++ data/launch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 WIREMOCK_JAR='wiremock.jar'
+EXTENSION_JAR='extension.jar'
+MAIN_CLASS='com.github.tomakehurst.wiremock.standalone.WireMockServerRunner'
+
+EXTENSION='ApiScenarioTransformer'
+
 PORT=8082
 
 # launch wiremock
-java -jar $WIREMOCK_JAR --extensions=com.smartsheet.wiremock.extensions.ApiScenarioTransformer --port=$PORT
+java  -cp "$WIREMOCK_JAR:$EXTENSION_JAR" $MAIN_CLASS --extensions=$EXTENSION  --port=$PORT
