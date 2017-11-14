5ecaef42f2df8a3d392378472823d7efe069483b
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-11-14 15:47:16 -0800

    Add sdk tests package to repo

--- package.sh
+++ package.sh
@@ -3,29 +3,18 @@
 # exit on error
 set -e
 
-PACKAGE_NAME=$1
-EXTENSION_JAR=$2
+EXTENSION_JAR=$1
 
+PACKAGE_NAME='sdk_tests_package'
 DATA_SCENARIO_DIR='data/scenarios'
 LAUNCH_SCRIPT='data/launch.sh'
-INSTALL_WIREMOCK_SCRIPT='data/install_wiremock.sh'
 PACKAGE_README='data/README.md'
 STUB_DEFAULTS='data/stub_defaults.json'
 
-INVALID_INPUT=0
-if [ -z "$PACKAGE_NAME" ]; then
-    echo 'Package name must be specified'
-    INVALID_INPUT=1
-fi
-
 if [ -z "$EXTENSION_JAR" ] || [ ! -f "$EXTENSION_JAR" ]; then
+    echo 'Usage: $0 diff-extension.jar'
     echo 'Could not find extension JAR'
-    INVALID_INPUT=1
-fi
 
-USAGE_TEXT="Usage: ./$0 package-name wiremock-standalone.jar diff-extension.jar"
-if [ $INVALID_INPUT == 1 ]; then
-    echo $USAGE_TEXT
     exit 1
 fi
 
@@ -56,9 +45,6 @@ cp "$EXTENSION_JAR" "$PACKAGE_NAME/libs/"
 
 # add launch script
 cp "$LAUNCH_SCRIPT" "$PACKAGE_NAME/launch.sh"
-
-# add install wiremock script
-cp "$INSTALL_WIREMOCK_SCRIPT" "$PACKAGE_NAME/install_wiremock.sh"
 
 # create zip, if zip is defined
 if command -v zip >/dev/null 2>&1; then
