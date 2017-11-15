803733e0b68ed2d8938770814c594ab585854f10
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-11-15 11:29:50 -0800

    Actually fix it

--- travis_scripts/install_wiremock.sh
+++ travis_scripts/install_wiremock.sh
@@ -3,8 +3,7 @@
 # exit on error
 set -e
 
-INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WIREMOCK_DOWNLOAD="$INSTALL_SCRIPT_DIR/../sdk_tests_package.zip"
+WIREMOCK_DOWNLOAD="smartsheet-sdk-tests/sdk_tests_package.zip"
 WIREMOCK_INSTALL_DIR='Smartsheet-WireMock-Bundle'
 
 
