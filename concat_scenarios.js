c2e5e09d2f16067b8987dcde92c4ea6707736201
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2018-03-09 09:50:59 -0800

    Add check to concatenate scenarios to verify that there are no duplicate scenarios

--- concat_scenarios.js
+++ concat_scenarios.js
@@ -12,6 +12,7 @@ var argv = require('yargs')
 
 var scenarios = loadScenarios(argv.scenarios);
 var flat_scenarios = _.flatten(scenarios, true);
+assertNoDuplicates(flat_scenarios);
 fs.writeFileSync(argv.output, JSON.stringify(flat_scenarios));
 
 
@@ -20,4 +21,30 @@ function loadScenarios(scenarioDirectory) {
     scenarioPaths = scenarioFilenames.map(fn => scenarioDirectory + '/' + fn);
 
     return scenarioPaths.map(path => JSON.parse(fs.readFileSync(path)));
+}
+
+function assertNoDuplicates(scenarios) {
+    var duplicates = getDuplicateElements(scenarios, (s1, s2) => s1.scenario === s2.scenario);
+
+    _.each(duplicates, (d) => console.log(`ERROR: Duplicate scenario: ${d.scenario}`));
+
+    if (duplicates.length > 0) {
+        console.log("Scenarios were not concatenated. Please fix scenarios and try again.");
+        process.exit(1);
+    }
+}
+
+function getDuplicateElements(arr, key) {
+    var duplicateElements = [];
+    _.each(arr, (element, index) => {
+        if (arrContains(arr.slice(index + 1), element, key) && !arrContains(duplicateElements, element, key)) {
+            duplicateElements.push(element);
+        }
+    });
+
+    return duplicateElements;
+}
+
+function arrContains(arr, element, key) {
+    return arr.findIndex((e) => key(element, e)) !== -1;
 }
