90e5a5677bd27decfceecff47c8c26c80eddd327
Tim Wells <timwells@westtek.com>
2019-08-13 15:49:10 -0700

    export java options

--- sdk_tests_package/launch.sh
+++ sdk_tests_package/launch.sh
@@ -6,7 +6,7 @@ EXTENSION='ApiScenarioTransformer'
 
 PORT=8082
 
-JDK_JAVA_OPTIONS='--add-modules java.xml.bind'
+export JDK_JAVA_OPTIONS='--add-modules java.xml.bind'
 
 # launch wiremock
 java  -cp "libs/*" $MAIN_CLASS --extensions=$EXTENSION --port=$PORT
